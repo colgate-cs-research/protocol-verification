51e92490e84d79282647ab0516518aa8e64f6527
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2020-07-16 10:51:24 -0400

    Fix import and usage of scapy_ospf

--- frrouting_automation/scapy_scripts/hello.py
+++ frrouting_automation/scapy_scripts/hello.py
@@ -1,4 +1,5 @@
 import scapy.all as scapy
+from scapy_ospf import OSPF_Hdr, OSPF_Hello
 
 packet = scapy.Ether(src='00:06:28:b9:85:31',dst='01:00:5e:00:00:05')
 
@@ -11,7 +12,7 @@ packet = packet/scapy.Dot1Q(vlan=33)
 
 #packet.show()
 packet = packet/scapy.IP(src='192.16.2.2',dst='172.17.0.5')
-packet = packet/scapy.OSPF_Hdr(src='192.16.2.2')
-packet = packet/scapy.OSPF_Hello(router='172.17.2.2',backup='172.17.2.1',neighbor='172.17.2.1')
+packet = packet/OSPF_Hdr(src='192.16.2.2')
+packet = packet/OSPF_Hello(router='172.17.2.2',backup='172.17.2.1',neighbor='172.17.2.1')
 packet.show()
 scapy.sendp(packet,loop=True,inter=0.1)
