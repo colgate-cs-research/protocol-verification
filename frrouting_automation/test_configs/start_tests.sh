34edc7bf62df0e746aca980b34d0ed0f7b17624f
omshii <omshisamal72@gmail.com>
2021-03-30 14:20:50 -0400

    removed sudo, added single router topo

--- frrouting_automation/test_configs/start_tests.sh
+++ frrouting_automation/test_configs/start_tests.sh
@@ -1,29 +1,61 @@
-#!/bin/bash 
+#!/bin/bash
 
-for filename in configs_temp/*; do
+declare -i starttime
+declare -i endtime
+declare -i dockertime
+declare -i conttime
+declare -i testtime
+declare -i copytime
+
+dockertime=0
+conttime=0
+testtime=0
+copytime=0
+
+starttime=0
+endtime=0
+
+for filename in configs/*; do
     
-    sudo python3 frr_config.py -c triangle_bgp.json -a stop
-    sudo python3 frr_config.py -c triangle_bgp.json -a start -b $filename
+    starttime=$(date +%s)
+    python3 frr_config.py -c single_bgp.json -a stop
+    python3 frr_config.py -c single_bgp.json -a start -b $filename
+    endtime=$(date +%s)
+    dockertime=$(($dockertime+$endtime-$starttime))
     
     #Get container id
+    starttime=$(date +%s)
     containers=$(docker ps)
     [[ $containers =~ [a-z0-9]{12} ]]
     container_id=$BASH_REMATCH
-
+    endtime=$(date +%s)
+    conttime=$(($conttime+$endtime-$starttime))
+    
     #Go to docker container and run tests
+    starttime=$(date +%s)
     docker cp create_expect.py $container_id:/etc/frr
     docker exec -i $container_id bash < in_docker.sh
-
+    endtime=$(date +%s)
+    testtime=$(($testtime+$endtime-$starttime))
+    
     #Copy test logs, generated configs, and expect tests from docker container
+    starttime=$(date +%s)
     file=$(basename $filename)
     docker cp $container_id:/etc/frr/tests.log logs/
-    sudo mv logs/tests.log logs/$file.tests.log
+    mv logs/tests.log logs/$file.tests.log
     docker cp $container_id:/etc/frr/bgpd.conf configs_generated/
-    sudo mv configs_generated/bgpd.conf configs_generated/$file.conf
+    mv configs_generated/bgpd.conf configs_generated/$file.conf
     docker cp $container_id:/etc/frr/run_tests tests/
-    sudo mv tests/run_tests tests/$file.tests
+    mv tests/run_tests tests/$file.tests
+    endtime=$(date +%s)
+    copytime=$(($copytime+$endtime-$starttime))
 
 done
 
-sudo python3 frr_config.py -c triangle_bgp.json -a stop
+python3 frr_config.py -c single_bgp.json -a stop
+
+echo "Setting up routers" $dockertime
+echo "Getting container info" $conttime
+echo "Making/Running tests" $testtime
+echo "Copying file" $copytime
 
