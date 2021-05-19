6dd0b12506d2b46caedd495e5273df217c1c950b
chase_jiang <xjiang@colgate.edu>
2021-05-18 22:23:08 -0400

    added router correspondence to prevent cross-router send/recv | added packet tracing for specific lsa causal pairs

--- protocol_implementation_interoperability/comprehensive/causal.py
+++ protocol_implementation_interoperability/comprehensive/causal.py
@@ -198,38 +198,57 @@ def double(fname):
     return r1, r2
 
 def run(final_result):
+    # send -> receive
     send_dict = defaultdict(set)
+    # receive -> send
     recv_dict = defaultdict(set)
+    # send -> receive
+    timestamp_trace_send_recv = defaultdict(set)
+    # receive -> send
+    timestamp_trace_recv_send = defaultdict(set)
+    #iterate through topologies
     for topologies in final_result:
+        #iterate through logs in each topology
         for logs in topologies:
+            # iterate through all msgs in each log
             for k in range(len(logs)):
+                #after sending a packet
                 if "Send" in logs[k]:
                     current_msg = logs[k]
                     send_time = re.search('Send at (.*) to ', current_msg)
                     send_time = float(send_time.group(1))
+                    curr_t_router = current_msg.split("to ")[1]
+                    #finding first msg received
                     for l in range(k,len(logs)):
                         next_msg = logs[l]
                         if "Receive" in next_msg:
+                            next_t_router = next_msg.split("from ")[1]
                             recv_time = re.search('Receive at (.*) from ', next_msg)
                             recv_time = float(recv_time.group(1))
-                            if (recv_time - send_time)>6:
+                            #appending to causal if condition is met: time diff > 6 and communicating routers are identical
+                            if (recv_time - send_time)>6 and curr_t_router == next_t_router:
+                                timestamp_trace_send_recv[current_msg.split(" to ")[0]].add(next_msg.split(" from ")[0])
                                 send_dict[current_msg.split("Send")[0]].add(next_msg.split("Receive")[0])
                                 break
+                # same as above but after receiving a packet
                 else:
                     current_msg = logs[k]
                     recv_time = re.search('Receive at (.*) from ', current_msg)
                     recv_time = float(recv_time.group(1))
+                    curr_t_router = current_msg.split("from ")[1]
                     for l in range(k,len(logs)):
                         next_msg = logs[l]
                         if "Send" in next_msg:
+                            next_t_router = next_msg.split("to ")[1]
                             send_time = re.search('Send at (.*) to ', next_msg)
                             send_time = float(send_time.group(1))
-                            if (send_time - recv_time)>6:
+                            if (send_time - recv_time)>6 and curr_t_router == next_t_router:
+                                timestamp_trace_recv_send[current_msg.split(" from ")[0]].add(next_msg.split(" to ")[0])
                                 recv_dict[current_msg.split("Receive")[0]].add(next_msg.split("Send")[0])
                                 break
 
     with open ('output/causal.txt', 'w') as f:
-        f.write("List of packets can be received given last sent packet type"+"\n")
+        f.write("receive -> send\n")
         for key in recv_dict:
             f.write(key+"\n")
             f.write(str(recv_dict[key])+"\n")
@@ -242,14 +261,18 @@ def run(final_result):
             # f.write("\n")
             # #####
         f.write("###########################################################################################################################################################\n")
-        f.write("List of packets can be send given last received packet type"+"\n")
+        f.write("send -> receive\n")
         for key in send_dict:
             f.write(key+"\n")
             f.write(str(send_dict[key])+"\n")
-
+    
+    #  recv -> send
     specific_causal_recv_sn = defaultdict(set)
+    #  send -> recv
     specific_causal_send_sn = defaultdict(set)
+    #  recv -> send
     specific_causal_recv_age = defaultdict(set)
+    #  send -> recv
     specific_causal_send_age = defaultdict(set)
     with open ('output/specific_causal.txt','w') as f:
         for key in recv_dict:
@@ -287,14 +310,34 @@ def run(final_result):
                                     # input relation values to be checked
                                     if first_packet_lssn[0] < second_packet_lssn[0]:
                                         specific_causal_recv_sn[key.split("/")[0]].add(i.split("/")[0])
+                                        # ######optional code to trace these paired packets
+                                        # for i1 in timestamp_trace_recv_send:
+                                        #     if first_packet_lst[0] in i1 and first_packet_lsid[0] in i1 and first_packet_ar[0] in i1 and first_packet_lssn[0] in i1:
+                                        #         for j1 in timestamp_trace_recv_send[i1]:
+                                        #             if second_packet_lst[0] in j1 and second_packet_lsid[0] in j1 and second_packet_ar[0] in j1 and second_packet_lssn[0] in j1:
+                                        #                 print(i1)
+                                        #                 print(j1)
+                                        #                 print()
+                                        #                 break
+                                        # ########
                                     if int(first_packet_age[0]) < int(second_packet_age[0]):
                                         specific_causal_recv_age[key.split("/")[0]].add(i.split("/")[0])
+                                        # ######optional code to trace these paired packets
+                                        # for i1 in timestamp_trace_recv_send:
+                                        #     if first_packet_lst[0] in i1 and first_packet_lsid[0] in i1 and first_packet_ar[0] in i1 and "AGE"+first_packet_age[0] in i1:
+                                        #         for j1 in timestamp_trace_recv_send[i1]:
+                                        #             if second_packet_lst[0] in j1 and second_packet_lsid[0] in j1 and second_packet_ar[0] in j1 and "AGE"+second_packet_age[0] in j1:
+                                        #                 print(i1)
+                                        #                 print(j1)
+                                        #                 print()
+                                        #                 break
+                                        # ########
 
 
 
         for key in send_dict:
             if "LS Update (4)" in key or "LS Acknowledge (5)" in key:
-                for i in recv_dict[key]:
+                for i in send_dict[key]:
                     if "LS Update (4)" in i or "LS Acknowledge (5)" in i:
                         #listing all lsa in the two packets
                         first_lsa_list = (key.split("/",1)[1]).split("#")
@@ -326,42 +369,69 @@ def run(final_result):
                                     # input relation values to be checked
                                     if first_packet_lssn[0] < second_packet_lssn[0]:
                                         specific_causal_send_sn[key.split("/")[0]].add(i.split("/")[0])
+                                        # ######optional code to trace these paired packets
+                                        # for i1 in timestamp_trace_send_recv:
+                                        #     if first_packet_lst[0] in i1 and first_packet_lsid[0] in i1 and first_packet_ar[0] in i1 and first_packet_lssn[0] in i1:
+                                        #         for j1 in timestamp_trace_send_recv[i1]:
+                                        #             if second_packet_lst[0] in j1 and second_packet_lsid[0] in j1 and second_packet_ar[0] in j1 and second_packet_lssn[0] in j1:
+                                        #                 print(i1)
+                                        #                 print(j1)
+                                        #                 print()
+                                        #                 break
+                                        # ########
                                     if int(first_packet_age[0]) < int(second_packet_age[0]):
                                         specific_causal_send_age[key.split("/")[0]].add(i.split("/")[0])
+                                        ######optional code to trace these paired packets
+                                        for i1 in timestamp_trace_send_recv:
+                                            if first_packet_lst[0] in i1 and first_packet_lsid[0] in i1 and first_packet_ar[0] in i1 and "AGE"+first_packet_age[0] in i1:
+                                                for j1 in timestamp_trace_send_recv[i1]:
+                                                    if second_packet_lst[0] in j1 and second_packet_lsid[0] in j1 and second_packet_ar[0] in j1 and "AGE"+second_packet_age[0] in j1:
+                                                        print(i1)
+                                                        print(j1)
+                                                        print()
+                                                        break
+                                        ########
                                         
-        f.write("--------------Given a packet is sent, packets to receive as a response with responding lsa containing greater sn."+"\n")
+        f.write("--------------Recv -> send, responding lsa containing greater sn."+"\n")
         for key in specific_causal_recv_sn:
             f.write(key+"\n")
             f.write(str(specific_causal_recv_sn[key])+"\n")
             f.write("\n")
-        f.write("--------------Given a packet is received, packets to send as a response with responding lsa containg greater sn"+"\n")
+        f.write("--------------send -> recv, responding lsa containg greater sn"+"\n")
         for key in specific_causal_send_sn:
             f.write(key+"\n")
             f.write(str(specific_causal_send_sn[key])+"\n")
             f.write("\n")
+        f.write("###########################################################################################################################################################\n")
         
-        f.write("--------------Given a packet is sent, packets to receive as a response with responding lsa containing greater age."+"\n")
+        f.write("--------------recv -> send, responding lsa containing greater age."+"\n")
         for key in specific_causal_recv_age:
             f.write(key+"\n")
             f.write(str(specific_causal_recv_age[key])+"\n")
             f.write("\n")
-        f.write("--------------Given a packet is received, packets to send as a response twith responding lsa containg greater age"+"\n")
+        f.write("--------------send -> recv, responding lsa containg greater age"+"\n")
         for key in specific_causal_send_age:
             f.write(key+"\n")
             f.write(str(specific_causal_send_age[key])+"\n")
             f.write("\n")
 
 
+
 def main():
     final_result = []
     ### delay is 3000 ms
-    files3 = ['logs/800_1_3.txt',
-    'logs/800_2_3.txt']
+    files3 = ['logs/l800_1_3.txt','logs/l800_2_3.txt',
+    'logs/l800_3_3.txt','logs/l800_4_3.txt',
+    'logs/l800_5_3.txt','logs/l800_6_3.txt',
+    'logs/l800_7_3.txt','logs/l800_8_3.txt',
+    'logs/l800_9_3.txt','logs/l800_10_3.txt',]
 
     for input_file3 in files3:
         final_result.append(triangle(input_file3))
 
-    files2 = ['logs/1000_1_2.txt']
+    files2 = ['logs/l1000_1_2.txt','logs/l1000_2_2.txt',
+    'logs/l1000_3_2.txt','logs/l1000_4_2.txt',
+    'logs/l1000_5_2.txt']
     for input_file2 in files2:
         final_result.append(double(input_file2))
 
